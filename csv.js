bb7cabb5830b5dccf7dc705f79e8d1f50ea703c0
nerd of woluhem <95163385+intel386dx@users.noreply.github.com>
2023-05-24 21:33:37 +0700

    Minor changes
    There have been many less-perceivable minor changes.

--- csv.js
+++ csv.js
@@ -1,5 +1,33 @@
+/*
+ * CSV Parser in JavaScript + command-line interface
+ * Licensed under the MIT License
+ *
+ * Copyright (c) 2023 Hilman Ahwas A.
+ *
+ * Permission is hereby granted, free of charge, to any person ob-
+ * taining a copy  of this  software  and associated documentation 
+ * files (the “Software”), to deal in the Software without restri-
+ * ction, including  without limitation  the rights to  use, copy, 
+ * modify, merge, publish, distribute, sublicense, and/or sell co-
+ * pies of the  Software, and to permit persons to  whom the Soft-
+ * ware  is furnished to  do so, subject  to the following  condi-
+ * tions:
+ * 
+ * The above copyright notice and this permission notice shall be 
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+ * OF MERCHANTABILITY, FITNESS  FOR A PARTICULAR  PURPOSE AND NON-
+ * INFRINGEMENT. IN NO  EVENT SHALL THE AUTHORS  OR COPYRIGHT HOL-
+ * DERS BE LIABLE FOR ANY CLAIM,  DAMAGES OR OTHER LIABILITY, WHE-
+ * THER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN  CONNECTION WITH THE SOFTWARE OR  THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ */
+ 
 // The global CSV object containing methods for converting CSV to multi-dimensional array and vice versa
-// Has been successfully tested on Windows Script Host
+// Has been tested on Windows Script Host
 var CSV = {
     // Pass your CSV data as the first argument
     parse: function(csv) {
@@ -72,7 +100,7 @@ function arrayToHTML(arr, head) {
 
 /* This can also act as a command-line tool that is compatible with both cscript (Windows) and Node.js (all platforms).
  * The purpose of this tool is to convert the CSV to JSON.
- * Here's how:
+ * Here's how it works:
  * 1. Run the script:
  *    a. Windows Script Host:
  *    cscript csv.js [ help | row_number column_number [ file ] ]
@@ -103,6 +131,7 @@ function arrayToHTML(arr, head) {
  * > The output would be:
  *   e
  */
+// This is the command-line interface. Only if it is not running on Node.js or WSH (Windows Script Host).
 if (typeof WScript != "undefined" || typeof process != "undefined") (function() {
     // Polyfills the ``console`` methods for Windows Script Host
     if (typeof console == "undefined" && typeof WScript != "undefined") console = {
@@ -137,7 +166,7 @@ if (typeof WScript != "undefined" || typeof process != "undefined") (function() 
 		          callback(y);
 		      });
 		    } else {
-		        callback(new Error("Only supports Node.js and Windows Script Host."));
+		        error(new Error("Only supports Node.js and Windows Script Host."));
 		    }
 		};
         // This function obtains the arguments given to the script.
@@ -152,7 +181,7 @@ if (typeof WScript != "undefined" || typeof process != "undefined") (function() 
                 throw new Error("We don't know what kind of environment this script is running on. We only know of Node.js and Windows Script Host.");
             };
         };
-        // This function exits the script with an error code.
+        // This function exits the script with an error code. If none is specified then it will exit with code zero.
         function exit(code) {
             if (typeof WScript != "undefined") {
                 WScript.Quit(!!code? code : 0);
@@ -163,16 +192,17 @@ if (typeof WScript != "undefined" || typeof process != "undefined") (function() 
         // This function prints the help message.
         function printHelp() {
             console.log(
-                "Usage\r\n" +
-                "csv.js [ help | row_number column_number [ file ] ]\r\n" + 
+                "Usage:\r\n" +
+                (typeof process != "undefined"? "node " : typeof WScript != "undefined"? "cscript " : "") + 
+                "csv.js [ help | <row_number> <column_number> [ <file> ] ]\r\n" + 
                 "\r\n" + 
-                "         help : Displays this help message.\r\n" + 
-                "   row_number : Specifies the target line index (row). It starts from 0.\r\n" +
-                "column_number : Specifies the target value (column). It starts from 0.\r\n" + 
-                "         file : A file to read from. If empty it will read from stdin." +
-                "                (Experimental feature: may break on Windows Script Host)\r\n" +
+                "           help : Displays this help message.\r\n" + 
+                "   <row_number> : Specifies the target line index (row). It starts from 0.\r\n" +
+                "<column_number> : Specifies the target value (column). It starts from 0.\r\n" + 
+                "         <file> : A file to read from. If empty it will read from stdin." +
+                "                  (Experimental feature: may break on Windows Script Host)\r\n" +
                 "\r\n" +
-                "This script reads the CSV data from standard input.\r\n"
+                "This script reads the CSV data from either a file or the standard input.\r\n"
             );
         };
         args = getCmdArgs();
@@ -185,6 +215,7 @@ if (typeof WScript != "undefined" || typeof process != "undefined") (function() 
             var lineIndex = args[0];
             var valueIndex = args[1];
             if (args[2] == "undefined") {
+            	// Input would be from stdin. May break on Windows Script Host.
 	            if (typeof WScript != "undefined") {
 	                var stdin = WScript.StdIn;
 	                while (!stdin.AtEndOfStream) {
@@ -206,11 +237,34 @@ if (typeof WScript != "undefined" || typeof process != "undefined") (function() 
 	                });
 	            };
             } else {
+            	/* Input would be from a file. This is a lot safer to work with.
+                 * How to Use This Method Instead:
+                 * 1. Redirect the program's CSV output from stdin/stderr to a file.
+                 *    If you want to read from an already existing file, skip this step.
+                 *    > For example: Echo an example CSV data and redirect it to a file named example.csv:
+                 *      echo 1,2,3 > example.csv
+                 *    > Note: 1. If you want only stderr, (assuming the echo command outputs to stderr,) use:
+                 *               echo 1,2,3 2> example.csv
+                 *            2. If you want stdout and stderr combined, use:
+                 *               echo 1,2,3 > example.csv 2>&1
+                 * 2. Now run this script like this:
+                 *    > For example: Get the 2nd column of the 1st row of the CSV data in example.csv:
+                 *      a. Windows Script Host
+                 *      cscript csv.js 0 1 example.csv
+                 *      b. Node.js
+                 *      node csv.js 0 1 example.csv
+                 * 3. If you don't want it for a long-term use, you may delete it using the command:
+                 *    a. Linux/Unix
+                 *    rm example.csv
+                 *    b. Windows
+                 *    del example.csv
+                 */
             	readFileAsText(args[2].toString(), function (x) {
             	    console.log(CSV.parse(x)[lineIndex][valueIndex]);
 	                exit(0);
                 }, function(x) {
-                	console.log(x.toString());
+                	console.error(x.toString());
+                    exit(1);
                 });
             };
         } else {
